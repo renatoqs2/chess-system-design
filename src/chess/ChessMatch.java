45acd99c79727d265eb19792744cb7d723595d84
renatoqs2 <renatoqs2@gmail.com>
2020-06-12 19:06:01 -0300

    temp commit

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -14,7 +14,7 @@ public class ChessMatch {
 		initialSetup(); 
 	}
 
-	public ChessPiece[][] getPieces() {
+	public ChessPiece[][] getPieces() {	
 		ChessPiece[][] mat = new ChessPiece[board.getRows()][board.getColumns()];
 		for (int i = 0; i < board.getRows(); i++) {
 			for (int j = 0; j < board.getColumns(); j++) {
